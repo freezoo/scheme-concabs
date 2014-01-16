00c8101a4996b41ff7bef06ce99adf5384ae2f6c
Heitor Chang <heitorchang@gmail.com>
2014-01-15 22:29:14 -0200

    predicates

--- recursion.scm
+++ recursion.scm
@@ -40,3 +40,28 @@
 (print-sign -3)
 (eq? 'Neg 'neg)
 
+(define (pair-copy pr)
+  (cons (car pr) (cdr pr)))
+(define lst '(1 2 3 4))
+(define lst-copy (pair-copy lst)
+(set-car! (cdr lst-copy) 99)
+lst
+(define lst-app (append lst))
+(set-car! (cdr lst) 33)
+lst-app
+
+;; append copies all but last list
+(define lst1 '(1 2 3))
+(define lst2 '(10 20 30))
+(define lst12 (append lst1 lst2 '()))
+st12
+(set-car! lst1 11)
+lst1
+(set-car! lst2 1010)
+
+(define (rev lst)
+  (if (null? lst)
+      '()
+      (append (reverse (cdr lst))
+              (list (car lst)))))
+(rev '(2 4 6 8 10))
